988bd1160ebda3e72c155f830a981008b16de56d
Maks <orbita4501@gmail.com>
2023-01-29 21:24:40 +0300

    done

---
+++ задание 36/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] masiv = new int[12]; // объявляем массив
+int s = 0;
+Random rand = new Random(); // метод
+
+// заполняем массив случайными числами
+ for (int i = 0; i < masiv.Length; i++)
+      {
+          masiv[i] = rand.Next(0,50);
+      }           
+            
+ //Вывод массива         
+ Console.Write("Массив: ");
+ for(int i = 0; i<masiv.Length; i++)
+Console.Write(" " + masiv[i]); 
+
+for(int i = 0; i<masiv.Length; i++)
+{
+if (i%2!=0)
+s = s+masiv[i];
+}
+Console.WriteLine(".");
+Console.WriteLine($"Сумма чисел: {s}");
