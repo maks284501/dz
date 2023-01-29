81aed378bc517b29a2cbbfb58df1bbd7dbae1301
Maks <orbita4501@gmail.com>
2023-01-29 21:19:16 +0300

    done

---
+++ задание 34/Program.cs
@@ -0,0 +1,22 @@
+﻿ int[] masiv = new int[12]; // объявляем массив
+int s = 0;
+Random rand = new Random(); // метод
+
+// заполняем массив случайными числами
+ for (int i = 0; i < masiv.Length; i++)
+      {
+          masiv[i] = rand.Next(100, 999);
+      }           
+            
+ //Вывод массива         
+ Console.Write("Array: ");
+ for(int i = 0; i<masiv.Length; i++)
+Console.Write(" " + masiv[i]); 
+
+for(int i = 0; i<masiv.Length; i++)
+{
+if (masiv[i]%2==0)
+s = s+1;
+}
+Console.WriteLine(".");
+Console.WriteLine($"Сумма четных: {s}");
